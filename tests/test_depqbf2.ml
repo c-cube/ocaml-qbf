c0f9d27481afd09dbf459cb0c2b88d2da3bd508c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-02-10 10:40:15 +0100

    update tests

--- tests/test_depqbf2.ml
+++ tests/test_depqbf2.ml
@@ -1,25 +1,27 @@
 
 module D = Depqbf
 
+let mk = Qbf.Lit.make
+
 let add_clause s l =
-  List.iter (D.add s) l;
-  D.add s 0
+  List.iter (fun x -> D.add s (mk x)) l;
+  D.add0 s
 
 let () =
   let s = D.create () in
   let level1 = D.new_scope s Qbf.Forall in
-  D.add s 1;
-  D.add s 2;
-  D.add s 0;
+  D.add s (mk 1);
+  D.add s (mk 2);
+  D.add0 s;
   let _level2 = D.new_scope s Qbf.Exists in
-  D.add s 4;
-  D.add s 5;
-  D.add s 0;
-  D.add_var_to_scope s 3 level1;
+  D.add s (mk 4);
+  D.add s (mk 5);
+  D.add0 s;
+  D.add_var_to_scope s (mk 3) level1;
   D.gc s;
   add_clause s [1;2;3];
   add_clause s [1;-2;3];
   add_clause s [-3];
   let res = D.check s in
-  Format.eprintf "res: %a@." Qbf.pp_result res;
+  Format.eprintf "(res: %a)@?" Qbf.pp_result res;
   ()
