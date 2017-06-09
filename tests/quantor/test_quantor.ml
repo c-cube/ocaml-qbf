c-cube/ocaml-qbf

commit ef458c77ded05521440f68b14842c9d0f699964b

makefile,oasis: move pre/post commands to makefile
In _oasis, mkdir -p wasn't working. I just moved it to Makefile
In quantor's ./configure, the printf(".....\n") were printing ^M
characters (yeah, windows native exec is printing CRLF characters).
I just had to remove the ending \n as they were not absolutely needed.

libs/quantor-3.2/configure
@@ -396,23 +396,25 @@ cat <<EOF >$tmp/getword.c
 int main (void)
 {
   if (sizeof(unsigned) == sizeof(void*))
-    printf ("unsigned\n");
+    printf ("unsigned");
   else if (sizeof(unsigned long) == sizeof(void*))
-    printf ("unsigned long\n");
+    printf ("unsigned long");
   else
-    printf ("invalid\n");
+    printf ("invalid");
   return 0;
 }
 EOF
 $RAWCC $RAWCFLAGS -o $tmp/getword $tmp/getword.c
 WORD="`$tmp/getword`"
-if [ "$WORD" = invalid ]
-then
-  echo
-  die "could not get word size"
-fi
-else
-WORD=unsigned
+case "$WORD" in
+  *invalid*)
+    echo
+    die "could not get word size"
+    ;;
+  *)
+    WORD=unsigned
+    ;;
+esac
 fi
 echo " $WORD"
 #*------------------------------------------------------------------------*#
@@ -439,9 +441,9 @@ then
 int main (void)
 {
   if (sizeof (unsigned long) == 8)
-    printf ("unsigned long\n");
+    printf ("unsigned long");
   else
-    printf ("invalid\n");
+    printf ("invalid");
   return 0;
 }
 EOF
@@ -454,9 +456,9 @@ EOF
 int main (void)
 {
   if (sizeof (unsigned long long) == 8)
-    printf ("unsigned long long\n");
+    printf ("unsigned long long");
   else
-    printf ("invalid\n");
+    printf ("invalid");
 }
 EOF
     if $RAWCC $RAWCFLAGS -o $tmp/getsignature $tmp/getsignature.c \
@@ -499,7 +501,7 @@ then
 int main (void)
 {
   if (sizeof(long) == 2 * sizeof(int))
-    printf ("long\n");
+    printf ("long");
   return 0;
 }
 EOF
@@ -514,7 +516,7 @@ EOF
 int main (void)
 {
   if (sizeof(long long) == 2 * sizeof(int))
-    printf ("long long\n");
+    printf ("long long");
   return 0;
 }
 EOF
@@ -587,7 +589,7 @@ main (void)
   for (alignment = 0; alignment <= 64; alignment++)
     if (is_power_of_two (base + alignment))
       {
-	printf ("%d\n", alignment);
+	printf ("%d", alignment);
 	exit (0);
       }
 

Makefile
@@ -42,8 +42,15 @@ configure:
 
 deps:
 	cd libs ; make
+	mkdir -p _build
+	cp libs/quantor*/libquantor.a libs/picosat*/libpicosat.a _build
+
 
 clean-deps:
 	cd libs ; make clean
+
+install-deps:
+	cp libs/quantor*/libquantor.a $$(ocamlfind query qbf)
+	cp libs/picosat*/libpicosat.a $$(ocamlfind query qbf)
 
 .PHONY: deps

_oasis
@@ -36,10 +36,6 @@ Library "qbf"
   Path:                 src/
   Modules:              Qbf
 
-if flag(quantor)
-    PostConfCommand:    make deps
-    PostDistcleanCommand: make clean-deps
-
 Library "qbf-quantor"
   Path:                 src/quantor/
   Modules:              Quantor
@@ -54,8 +50,9 @@ Library "qbf-quantor"
 
 # magiiiic so that '-lquantor' works
 if flag(quantor)
-    PreBuildCommand:    mkdir -p _build && cp libs/quantor-3.2/libquantor.a libs/picosat-960/libpicosat.a _build
-    PostInstallCommand: cp libs/quantor-3.2/libquantor.a libs/picosat-960/libpicosat.a `ocamlfind query qbf`
+    PreBuildCommand:    make deps
+    PostCleanCommand:   make clean-deps
+    PostInstallCommand: make install-deps
 
 Library "qbf-depqbf"
   Path:                 src/depqbf/

setup.ml
@@ -1,7 +1,7 @@
 (* setup.ml generated for the first time by OASIS v0.4.5 *)
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: f38d50fcb2273b0971746e9b8b5f83bf) *)
+(* DO NOT EDIT (digest: 16fd35c984fe9ac6965c6399c0c2a695) *)
 (*
    Regenerated by OASIS v0.4.9~HEAD
    Visit http://oasis.forge.ocamlcore.org for more information and
@@ -8189,11 +8189,7 @@ let setup_t =
           conf_custom =
             {
                pre_command = [(OASISExpr.EBool true, None)];
-               post_command =
-                 [
-                    (OASISExpr.EBool true, None);
-                    (OASISExpr.EFlag "quantor", Some (("make", ["deps"])))
-                 ]
+               post_command = [(OASISExpr.EBool true, None)]
             };
           build_type = (`Build, "ocamlbuild", Some "0.4");
           build_custom =
@@ -8201,18 +8197,7 @@ let setup_t =
                pre_command =
                  [
                     (OASISExpr.EBool true, None);
-                    (OASISExpr.EFlag "quantor",
-                      Some
-                        (("mkdir",
-                           [
-                              "-p";
-                              "_build";
-                              "&&";
-                              "cp";
-                              "libs/quantor-3.2/libquantor.a";
-                              "libs/picosat-960/libpicosat.a";
-                              "_build"
-                           ])))
+                    (OASISExpr.EFlag "quantor", Some (("make", ["deps"])))
                  ];
                post_command = [(OASISExpr.EBool true, None)]
             };
@@ -8224,15 +8209,7 @@ let setup_t =
                  [
                     (OASISExpr.EBool true, None);
                     (OASISExpr.EFlag "quantor",
-                      Some
-                        (("cp",
-                           [
-                              "libs/quantor-3.2/libquantor.a";
-                              "libs/picosat-960/libpicosat.a";
-                              "`ocamlfind";
-                              "query";
-                              "qbf`"
-                           ])))
+                      Some (("make", ["install-deps"])))
                  ]
             };
           uninstall_custom =
@@ -8243,17 +8220,17 @@ let setup_t =
           clean_custom =
             {
                pre_command = [(OASISExpr.EBool true, None)];
-               post_command = [(OASISExpr.EBool true, None)]
-            };
-          distclean_custom =
-            {
-               pre_command = [(OASISExpr.EBool true, None)];
                post_command =
                  [
                     (OASISExpr.EBool true, None);
                     (OASISExpr.EFlag "quantor",
                       Some (("make", ["clean-deps"])))
                  ]
+            };
+          distclean_custom =
+            {
+               pre_command = [(OASISExpr.EBool true, None)];
+               post_command = [(OASISExpr.EBool true, None)]
             };
           plugins =
             [(`Extra, "META", Some "0.4"); (`Extra, "DevFiles", Some "0.4")];
@@ -8262,8 +8239,7 @@ let setup_t =
        };
      oasis_fn = Some "_oasis";
      oasis_version = "0.4.9~HEAD";
-     oasis_digest =
-       Some "\208\166\153W\231\1902\179\249~\179\212\191[\029\174";
+     oasis_digest = Some "\215x\0058l\156 \186\024\209;\007O\216\018\147";
      oasis_exec = None;
      oasis_setup_args = [];
      setup_update = false
@@ -8271,7 +8247,7 @@ let setup_t =
 
 let setup () = BaseSetup.setup setup_t;;
 
-# 8275 "setup.ml"
+# 8251 "setup.ml"
 let setup_t = BaseCompat.Compat_0_4.adapt_setup_t setup_t
 open BaseCompat.Compat_0_4
 (* OASIS_STOP *)

commit be396191af65b461176363a66e6e53aeedea990a

fixed a regression bug created in quantor's configure
Bug introduced in ef458c77d.

libs/quantor-3.2/configure
@@ -411,10 +411,9 @@ case "$WORD" in
     echo
     die "could not get word size"
     ;;
-  *)
-    WORD=unsigned
-    ;;
 esac
+else
+WORD=unsigned
 fi
 echo " $WORD"
 #*------------------------------------------------------------------------*#

tests/quantor/test_quantor.ml
@@ -5,15 +5,25 @@ open Qbf.QFormula
 
 
 (*let a,b,c = (Lit.make 1, Lit.make 2, Lit.make 3)*)
-let test_quantor _ =
+let test_quantor_false _ =
     let a,b,c = (Lit.make 1, Lit.make 2, Lit.make 3) in
     let formula = and_l [atom a; atom b; atom c] in
-    let qcnf = QFormula.cnf (forall [a; b] (prop formula)) in
+    let qcnf = QFormula.cnf (forall [a; b; c] (prop formula)) in
+    let _ = solve Quantor.solver qcnf
+    in ()
+
+(* Maël: I added this test after discovering a bug I had introduced when
+   modifying the ./configure of quantor (line ~410). *)
+let test_quantor_true _ =
+    let a,b,c = (Lit.make 1, Lit.make 2, Lit.make 3) in
+    let formula = or_l [atom a; atom b; atom c] in
+    let qcnf = QFormula.cnf (exists [a; b; c] (prop formula)) in
     let _ = solve Quantor.solver qcnf
     in ()
 
 
 let () = run_test_tt_main (
 "quantor">:::[
-    "test_quantor">::(test_quantor);
+    "test_quantor_false">::(test_quantor_false);
+    "test_quantor_true">::(test_quantor_true);
 ])
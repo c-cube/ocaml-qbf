be396191af65b461176363a66e6e53aeedea990a
Maël Valais <mael.valais@gmail.com>
2017-06-09 11:27:47 +0200

    fixed a regression bug created in quantor's configure
    Bug introduced in ef458c77d.

--- tests/quantor/test_quantor.ml
+++ tests/quantor/test_quantor.ml
@@ -5,15 +5,25 @@ open Qbf.QFormula
 
 
 (*let a,b,c = (Lit.make 1, Lit.make 2, Lit.make 3)*)
-let test_quantor _ =
+let test_quantor_false _ =
     let a,b,c = (Lit.make 1, Lit.make 2, Lit.make 3) in
     let formula = and_l [atom a; atom b; atom c] in
-    let qcnf = QFormula.cnf (forall [a; b] (prop formula)) in
+    let qcnf = QFormula.cnf (forall [a; b; c] (prop formula)) in
+    let _ = solve Quantor.solver qcnf
+    in ()
+
+(* Maël: I added this test after discovering a bug I had introduced when
+   modifying the ./configure of quantor (line ~410). *)
+let test_quantor_true _ =
+    let a,b,c = (Lit.make 1, Lit.make 2, Lit.make 3) in
+    let formula = or_l [atom a; atom b; atom c] in
+    let qcnf = QFormula.cnf (exists [a; b; c] (prop formula)) in
     let _ = solve Quantor.solver qcnf
     in ()
 
 
 let () = run_test_tt_main (
 "quantor">:::[
-    "test_quantor">::(test_quantor);
+    "test_quantor_false">::(test_quantor_false);
+    "test_quantor_true">::(test_quantor_true);
 ])
